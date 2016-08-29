41127e469630b3c261dfe1341dc0f34ae49f07e0
Richard Kho <hello@richardkho.com>
2016-08-28 22:17:12 -0700

    More code samples added plus images

---
+++ 2. Advanced Tasks/7. ExpandableCell With Conditional Text/EditTask/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  editTaskContainer: {
+    flex: 1,
+    paddingTop: 64
+  },
+  editTaskText: {
+    fontSize: 36,
+    paddingTop: 65
+  },
+  editTextInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 10
+  },
+  datePicker: {
+    height: 0
+  }
+})
+
+export default styles;
